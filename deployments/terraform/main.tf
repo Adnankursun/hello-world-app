05c12b4365244c8398f954debaa5f3a0bde7cc45
adnan kursun <54333199+Adnankursun@users.noreply.github.com>
2020-09-12 11:54:42 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -14,7 +14,7 @@ output "application_endpoint" {
 
 variable  "deployment_image" {
 
-    default = "docker.bugdalorian.com/hello-world-app-dev-feature:	17ada4b"
+    default = "docker.bugdalorian.com/hello-world-app-dev-feature:17ada4b"
 
     default = "docker.bugdalorian.com/hello-world-app-dev-feature:dac201f"
 
